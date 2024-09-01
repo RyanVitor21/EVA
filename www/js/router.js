140e7bb051afd9db6aed7b6d9cb74373127376df
Ryan <ryanvitorconta2@gmail.com>
2024-09-01 20:52:06 -0300

    adicionando framework7

---
+++ www/js/router.js
@@ -0,0 +1,20 @@
+var app = new Framework7({
+    // App root element
+    el: '#app',
+    // App Name
+    name: 'EVA',
+
+    id: "com.eva",
+    // Enable swipe panel
+    panel: {
+      swipe: true,
+    },
+    // Add default routes
+    routes: [
+      {
+        path: '/about/',
+        url: 'about.html',
+      },
+    ],
+    // ... other parameters
+  });
